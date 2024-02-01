53c7b3fdb99413b82c04a6a56e3fa036b5b730d3
Kabura21 <94135443+Kabura21@users.noreply.github.com>
2024-02-01 21:38:30 +0100

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,239 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+     {
+     numb: 6,
+     question: "Šta je bitno za web dizajn?",
+     answer: "Za web dizajn je bitno dobro poznavanje HTML,CSS-a i Javascripta",
+     options: [
+       "Nije potrebno ništa",
+       "Potrebno je imati zanje iz C jezika",
+       "Za web dizajn je bitno dobro poznavanje HTML,CSS-a i Javascripta",
+       "Potrebno je poznavati kako web preglednici funkcionišu"
+     ]
+   },
+
+   {
+    numb: 7,
+    question: "Zašto je web dizajn jedan od najpopularnijih zanimanja?",
+    answer: "Zato što je svim firmama,korporacijama,prodavnicama potrebna web stranica",
+    options: [
+      "Nije jedan od najbitnijih zanimanja u IT sektoru",
+      "Zato što nije teško za razliku od softverskog inžinjera, i drugih zanimanja poput manipuliranja podacima",
+      "Zato što je to dio razvijajućeg IT sektora",
+      "Zato što je svim firmama,korporacijama,prodavnicama potrebna web stranica"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Je li moguće napraviti stranicu na drugi način?",
+    answer: "Da moguće je preko Wordpressa, Nicepage-a i drugih sličnih aplikacija",
+    options: [
+      "Nije moguće",
+      "Da moguće je preko Wordpressa, Nicepage-a i drugih sličnih aplikacija",
+      "Nemoguće je moguće je napraviti stranicu samo preko koda",
+      "Moguće je ali ne praktikuje se"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "Da li je moguće kodirati stranice u nekom drugom programerskom jeziku?",
+    answer: "Nije moguće kodirati stranicu u drugim programerskim jezicima stranice",
+    options: [
+      "Nije moguće kodirati stranicu u drugim programerskim jezicima stranice",
+      "Moguće je ali ne kodira se na isti način",
+      "Moguće je čak je i HTML omogućio python da se kodira u HTML dokumentu",
+      "Moguće je ali ne praktikuje se"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Po čemu se razlikuje web dizajn od ostalih djelatnosti u IT-u?",
+    answer: "Razlikuje se po fleksibilnosti, po tome što sve informacije i podatke dobijamo sa web stranica",
+    options: [
+      "Razlikuje se po tome što nije težak",
+      "Razlikuje se po fleksibilnosti, po tome što sve informacije i podatke dobijamo sa web stranica",
+      "Razlikuje se po tome što je moguće gledati na svim uređajima",
+      "Razlikuje se po mogućnosti da se može mijenjati svaki dan"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "Zašto je jako bitan CSS u modernom web dizajnu?",
+    answer: "Bitan je CSS zato što oblikuje i daje dizajn web stranici",
+    options: [
+      "Nije bitan zato što je dovoljan HTML",
+      "Bitan je CSS zato što oblikuje i daje dizajn web stranici",
+      "Nebitan je zato što imamo programe za izradu stranica",
+      "Bitno je zato što moguće da naša stranica bude pristupačna svim uređajima"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "Je li dovoljan HTML & CSS za izradu stranice?",
+    answer: "Za modernu i savremenu funkcionalnost i davanje dodatne mogućnosti vašoj stranici preporučuje se primjena Javascripta",
+    options: [
+      "Dovoljno je za one page stranicu",
+      "HTML & CSS daje dovoljne mogućnosti na stranici",
+      "Za modernu i savremenu funkcionalnost i davanje dodatne mogućnosti vašoj stranici preporučuje se primjena Javascripta",
+      "CSS daje fukcionalnost stranici"
+    ]
+  },
+
+  {
+    numb: 13,
+    question: "Zašto se spominje SQL i PHP u web dizajnu?",
+    answer: "Spominje se zato što PHP komunicira sa serverom, dok SQL služi za izradu bazu podataka",
+    options: [
+      "Spominje se zato što daje mogućnost komuniciranja korisnika sa drugim korisnicma stranice",
+      "Spominje se zato što PHP komunicira sa serverom, dok SQL služi za izradu bazu podataka",
+      "Spominje se zato što komplikovanije stranice sa chatom, prikazivanjem sadržaja i šta drugi korisnici rade u datom momentu",
+      "SQL nije potreban u web dizajniranju"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "Zašto se spominje SQL i PHP u web dizajnu?",
+    answer: "Spominje se zato što PHP komunicira sa serverom, dok SQL služi za izradu bazu podataka",
+    options: [
+      "Spominje se zato što daje mogućnost komuniciranja korisnika sa drugim korisnicma stranice",
+      "Spominje se zato što PHP komunicira sa serverom, dok SQL služi za izradu bazu podataka",
+      "Spominje se zato što komplikovanije stranice sa chatom, prikazivanjem sadržaja i šta drugi korisnici rade u datom momentu",
+      "SQL nije potreban u web dizajniranju"
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "Šta je prvi tag u HTML dokumentu?",
+    answer: "<html>",
+    options: [
+      "<title>",
+      "<DOCTYPE html>",
+      "<head>",
+      "<html>",
+    ]
+  },
+
+  {
+    numb: 16,
+    question: "U kojem tagu se kodira sadržaj stranice?",
+    answer: "<body>",
+    options: [
+      "<body>",
+      "<head>",
+      "<header>",
+      "<footer>"
+    ]
+  },
+
+  {
+    numb: 17,
+    question: "kako se selektuju klase u CSS-u?",
+    answer: "putem .",
+    options: [
+      "putem ;",
+      "putem .",
+      "putem ,",
+      "putem -"
+    ]
+  },
+  
+
+  {
+    numb: 18,
+    question: "Koji je tag za slike?",
+    answer: "<img src=''>",
+    options: [
+      "<img src=''>",
+      "<image src=''",
+      "<slika src=''",
+      "<images src=''"
+    ]
+  },
+
+  {
+    numb: 19,
+    question: "Koji je tag za video?",
+    answer: "<video src=''>",
+    options: [
+      "<video src=''>",
+      "<videos src=''",
+      "<videoo src=''",
+      "<videji src=''"
+    ]
+  },
+
+  {
+    numb: 20,
+    question: "Koji je kod za umetanje fonta iz google fonts?",
+    answer: "@import url();",
+    options: [
+      "#import url();",
+      ".import url();",
+      "@amport url();",
+      "@import url();"
+    ]
+  },
+  
+];
